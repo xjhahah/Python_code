a36526f0fccb16c948e7ee13add1b3946df5f96e
xjhahah <908234867@qq.com>
2019-10-15 11:13:20 +0800

    add code

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-num = input("tring: ")
-print(num)
+#coding=utf-8
+
+#num = input("tring: ")
+#print(num)
+
+'''
+    Python 基础语法练习
+'''
+True,False = False,True
+if True:
+    print("haha")
+else:
+    print("hehe")
+
+for i in range(0,10):
+    print(i)
+print(i)
