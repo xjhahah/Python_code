b11c743a31080cc53208eb2d78d42cae8e4f1b83
xjhahah <908234867@qq.com>
2019-10-16 16:45:32 +0800

    add code

--- test.py
+++ test.py
@@ -1,17 +1,69 @@
-#coding=utf-8
+# coding=utf-8
+'''
+    1、橘色字体代表关键字
+    2、紫色字体代表函数对象
+'''
 
-#num = input("tring: ")
-#print(num)
+# 查看关键字
+import keyword
+import random
+
+# print(keyword.kwlist)
+
+#  r+'' 保留字符串原有格式  r == raw  原有的
+s1 = r'hello \n world'
+print(s1)
 
 '''
-    Python 基础语法练习
+# 变量类型：
+# int
+# float
+# bool
+# byte：字节，b'+字符串
+# list：列表 [] 允许重复元素
+# tuple：元组 () 允许重复元素
+# set：集合 {} 不允许又重复元素
+# dict：字典 {key:value}
 '''
-True,False = False,True
-if True:
-    print("haha")
-else:
-    print("hehe")
+sockets = {'ip': '192.168.0.1', 'port': 8080}
 
-for i in range(0,10):
-    print(i)
-print(i)
+print(sockets['ip'])
+
+# 运算符
+s = 'hello'
+ss = s + ' world'
+print(ss)
+s = 10
+print(s ** 3)
+
+a = 10
+print(id(a))  # 获取变量的地址
+
+b = 10
+print(id(b))
+print()
+print(a == b)  # 比较的是两个数的值
+print(a is b)  # 比较的是两个数的地址
+
+a, b = 10, 20
+print(a, b)
+
+# 简单的猜数字游戏
+random_num = random.randint(1, 10)
+
+time = 0
+
+while time < 3:
+    num = int(input("请输入你才猜的数字："))
+    if (num == random_num):
+        print("恭喜你找到了！")
+        print(random_num)
+        break
+    elif num < random_num:
+        print("猜小了。。")
+    else:
+        print("猜大了。。。")
+    time += 1
+if time == 3:
+    print('您要猜的数字：', random_num)
+    print("您的机会已用完，太菜了")
