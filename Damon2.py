89776b88b2e073276080ff8f37e2b7283510ed76
xjhahah <908234867@qq.com>
2019-10-22 22:26:21 +0800

    add string 操作 code

--- Damon2.py
+++ Damon2.py
@@ -5,7 +5,7 @@
 '''
 
 # 查找
-s = 'D:\PythonCode\Python_code\Damon2.py'
+s = r'D:\PythonCode\Python_code\Damon2.py'
 index = s.rfind('\\')
 print(index)
 print(s[index + 1:])
@@ -15,5 +15,8 @@ s = 'hello 帅哥，我是你爸爸。帅哥，我是你爷爷'
 s = s.replace('帅哥', '美女', 1)
 print(s)
 
-# 切割
+# 切割   spilt('分隔符'，最大分割次数（可选）) ---- 返回分割后的列表 ， 如果没有指定分割次数默认所有的都进行切割
+s = r'D:\PythonCode\Python_code\Damon2.py'
+list1 = s.split('\\')
 
+print(list1)
